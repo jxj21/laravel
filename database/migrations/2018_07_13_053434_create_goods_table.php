6c35d671f4b6396c21d4d73e1c00a8570873c8b5
jxj21 <jxj21@126.com>
2018-07-14 11:18:44 +0800

    first commit

---
+++ database/migrations/2018_07_13_053434_create_goods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->comment('标题');
+            $table->string('description')->comment('商品详情');
+            $table->float('price')->comment('价格');
+            $table->string('location')->comment('地点');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
